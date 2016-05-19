beebe02b4f36d36940318ac203903038a6f97d36
Fx Suarez <francoisxaviersuarez@gmail.com>
2016-05-19 11:46:59 +0200

    Beginning of work on the database emulator

---
+++ Omega/Omega.DataManager/RowTrackEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+namespace Omega.DataManager
+{
+    class RowTrackEntity : TableEntity
+    {
+        public RowTrackEntity( UserEntity user, PlaylistEntity playlist )
+        {
+            PartitionKey = user.email;
+            //RowKey = ;
+        }
+
+        public RowTrackEntity() { }
+
+        public string Title { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Omega/Omega.DataManager/UserEntity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omega.DataManager
+{
+    public class UserEntity : TableEntity
+    {
+        public UserEntity( string lastName, string firstName )
+        {
+            //PartitionKey = lastName;
+            //RowKey = firstName;
+        }
+
+        public UserEntity() { }
+
+        public string email { get; set; }
+        //Image image { get; set; }
+        public string IdFacebook_Spotify_Deezer { get; set; }
+    }
+}
