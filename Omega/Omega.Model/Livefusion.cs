c32cd890b2617621815e7a140a9cc3ee0354f4fd
ThibaultCam <cam@intechinfo.fr>
2016-06-07 09:35:32 +0200

    Livefusion almost over / need test

--- Omega/Omega.Model/Livefusion.cs
+++ Omega/Omega.Model/Livefusion.cs
@@ -1,52 +1,40 @@
-﻿using Omega.Crawler;
-using Omega.DataManager;
+﻿using Omega.DataManager;
+using System;
 using System.Collections.Generic;
 
 namespace Omega.Model
 {
     public class Livefusion
     {
-        public List<string> SelectSong(List<string> playlistIdSource, MetaDonnees askedDonnees)
+        public List<string> SelectSong(List<string> playlistIdSource, MetaDonnees askedDonnees, double ratio)
         {
-            Requests r = new Requests();
+            Requests cr = new Requests();
             List<string> FilteredList = new List<string>();
             List<CleanTrack> cleanTracks = new List<CleanTrack>();
-            for(int i= 0; i < playlistIdSource.Count; i++)
+            for (int i = 0; i < playlistIdSource.Count; i++)
             {
-                CleanTrack analysedSong = r.GetSongCleanTrack(playlistIdSource[i]);
-                if(askedDonnees.acousticness == null || askedDonnees.acousticness == analysedSong.Acousticness)
-                {
-                    if (askedDonnees.danceability == null || askedDonnees.danceability == analysedSong.Danceability)
-                    {
-                        if (askedDonnees.energy == null || askedDonnees.energy == analysedSong.Energy)
-                        {
-                            if (askedDonnees.instrumentalness == null || askedDonnees.instrumentalness == analysedSong.Instrumentalness)
-                            {
-                                if (askedDonnees.liveness == null || askedDonnees.liveness == analysedSong.Liveness)
-                                {
-                                    if (askedDonnees.loudness == null || askedDonnees.loudness == analysedSong.Loudness)
-                                    {
-                                        if (askedDonnees.mode == null || askedDonnees.mode == analysedSong.Mode)
-                                        {
-                                            if (askedDonnees.speechiness == null || askedDonnees.speechiness == analysedSong.Speechiness)
-                                            {
-                                                if (askedDonnees.tempo == null || askedDonnees.tempo == analysedSong.Tempo)
-                                                {
-                                                    if (askedDonnees.valence == null || askedDonnees.valence == analysedSong.Vanlence)
-                                                    {
-                                                        FilteredList.Add(analysedSong.Id);
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }               
+                CleanTrack analysedSong = cr.GetSongCleanTrack(playlistIdSource[i]);
+                if (Compare(askedDonnees.acousticness, analysedSong.Acousticness, ratio)
+                && Compare(askedDonnees.danceability, analysedSong.Danceability, ratio)
+                && Compare(askedDonnees.energy, analysedSong.Energy, ratio)
+                && Compare(askedDonnees.instrumentalness, analysedSong.Instrumentalness, ratio)
+                && Compare(askedDonnees.liveness, analysedSong.Liveness, ratio)
+                && Compare(askedDonnees.loudness, analysedSong.Loudness, ratio)
+                && Compare(askedDonnees.mode, analysedSong.Mode, ratio)
+                && Compare(askedDonnees.speechiness, analysedSong.Speechiness, ratio)
+                && Compare(askedDonnees.tempo, analysedSong.Tempo, ratio)
+                && Compare(askedDonnees.valence, analysedSong.Vanlence, ratio))
+                    FilteredList.Add(analysedSong.Id);
+            }
             return FilteredList;
+        }
+
+        public static bool Compare(string asked, string analysed, double ratio)
+        {
+            if (asked != null) asked = asked.Replace(".", ",");
+            if (analysed != null) analysed = analysed.Replace(".", ",");
+
+            return (asked == null || analysed == null || (Double.Parse(analysed) > Double.Parse(asked) - ratio && Double.Parse(analysed) < Double.Parse(asked) + ratio));
         }
     }
 }
