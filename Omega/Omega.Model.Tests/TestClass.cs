a52bf596be250c7b7d487e2d379188b812d730e8
ThibaultCam <cam@intechinfo.fr>
2016-06-21 13:46:53 +0200

    Lounge mode : done
    Dance mode : done

--- Omega/Omega.Model.Tests/TestClass.cs
+++ Omega/Omega.Model.Tests/TestClass.cs
@@ -35,7 +35,7 @@ namespace Omega.Model.Tests
         }
 
         [Test]
-        public void Open_And_Parse_Modes()
+        public void Get_Filtered_Playlist_Using_Ambiancer()
         {
             Ambiance a = new Ambiance();
             List<string> musiques = new List<string>();
@@ -49,10 +49,23 @@ namespace Omega.Model.Tests
             musiques.Add("s:3R8dCpuEH2QkkUETNBqmrH");
             musiques.Add("s:2NEAbdfS4laGWAETgYDeiY");
             musiques.Add("s:0TaHfsr7Gt5epPSeVIpwV6");
-            List<string> filteredList = a.Ambiancer("Lounge", musiques);
+            musiques.Add("s:0xbkxTqSaW5blsYgRXpB5I");
+            musiques.Add("s:7jQBORjiir0pNSKGaHevq9");
+            musiques.Add("s:3LmpQiFNgFCnvAnhhvKUyI");
+            musiques.Add("s:3s6ltUrI93LBKU8taezsLn");
+            musiques.Add("s:4vp2J1l5RD4gMZwGFLfRAu");
+            musiques.Add("s:2Foc5Q5nqNiosCNqttzHof");
+            musiques.Add("s:6Z8R6UsFuGXGtiIxiD8ISb");
+            musiques.Add("s:41UTnVa6DvcVPUYoXWA97h");
+            musiques.Add("s:69RoAhDqFOiQb2pQvb24Ii");
+            musiques.Add("s:6xMpUNOfaSkyywPiFFXZFh");
+            musiques.Add("s:27pNtiDgRXH9tZM1js0Njc");
+
+
+            List<string> filteredList = a.Ambiancer("Dance", musiques);
             foreach (string musique in filteredList)
             {
-                Console.WriteLine(musiques);
+                Console.WriteLine(musique);
             }
         }
 
@@ -70,18 +83,23 @@ namespace Omega.Model.Tests
                     Console.WriteLine("--------------------------------------");
                     Console.WriteLine(i.Key + ", " + i.Value);
                     Console.WriteLine(id);
-                    await c.TrackMetadonnee(id);
-                    Console.WriteLine("");
                     var tm = await c.TrackMetadonnee(id);
-                    Console.WriteLine("acousticness : " + tm.acousticness);
-                    Console.WriteLine("danceability : " + tm.danceability);
-                    Console.WriteLine("energy : " + tm.energy);
-                    Console.WriteLine("instrumentalness : " + tm.instrumentalness);
-                    Console.WriteLine("liveness : " + tm.liveness);
-                    Console.WriteLine("loudness : " + tm.loudness);
-                    Console.WriteLine("speechiness : " + tm.speechiness);
-                    Console.WriteLine("tempo : " + tm.tempo);
-                    Console.WriteLine("valence : " + tm.valence);
+                    if (double.Parse(tm.danceability.Replace(".",",")) > 0.7) {
+                        Console.WriteLine("");
+                        Console.WriteLine("acousticness : " + tm.acousticness);
+                        Console.WriteLine("danceability : " + tm.danceability);
+                        Console.WriteLine("energy : " + tm.energy);
+                        Console.WriteLine("instrumentalness : " + tm.instrumentalness);
+                        Console.WriteLine("liveness : " + tm.liveness);
+                        Console.WriteLine("loudness : " + tm.loudness);
+                        Console.WriteLine("speechiness : " + tm.speechiness);
+                        Console.WriteLine("tempo : " + tm.tempo);
+                        Console.WriteLine("valence : " + tm.valence);
+                    }
+                    else
+                    {
+                        Console.WriteLine("danceability : " + tm.danceability);
+                    }
                 }
             }
         }
@@ -89,22 +107,31 @@ namespace Omega.Model.Tests
         public Dictionary<string, string> ListeMusiqueLounge()
         {
             // Lounge
-            Dictionary<string, string> musiquesLounge = new Dictionary<string, string>();
-            //musiquesLounge.Add("Broke Inside My Mind", "Anitek");
-            //musiquesLounge.Add("Lounge", "Zen");
-            //musiquesLounge.Add("chill out", "Zen");
-            //musiquesLounge.Add("Talk To Me", "Miranda Shvangiradze");
-            //musiquesLounge.Add("zen lounge", "zen");
-            //musiquesLounge.Add("bar lounge", "bar lounge");
-            //musiquesLounge.Add("paris lounge", "bar lounge");
-            //musiquesLounge.Add("mood music", "bar lounge");
-            //musiquesLounge.Add("dani california", "red hot chili peppers");
-            musiquesLounge.Add("lose yourself", "eminem");
-            musiquesLounge.Add("mambo no. 5", "lou bega");
-            musiquesLounge.Add("stayin' alive", "bee gees");
-            musiquesLounge.Add("magic in the air", "magic system");
-            musiquesLounge.Add("i gotta feeling", "the black eyed peas");
-            return musiquesLounge;
+            //Dictionary<string, string> musiquesLounge = new Dictionary<string, string>();
+            ////musiquesLounge.Add("Broke Inside My Mind", "Anitek");
+            ////musiquesLounge.Add("Lounge", "Zen");
+            ////musiquesLounge.Add("chill out", "Zen");
+            ////musiquesLounge.Add("Talk To Me", "Miranda Shvangiradze");
+            ////musiquesLounge.Add("zen lounge", "zen");
+            ////musiquesLounge.Add("bar lounge", "bar lounge");
+            ////musiquesLounge.Add("paris lounge", "bar lounge");
+            ////musiquesLounge.Add("mood music", "bar lounge");
+            //return musiquesLounge;
+
+            Dictionary<string, string> musiquesDance = new Dictionary<string, string>();
+            musiquesDance.Add("requiem", "mozart");
+            musiquesDance.Add("les quatre saisons, l'été", "vivaldi");
+            //musiquesDance.Add("valse", "evgeny grinko");
+            //musiquesDance.Add("du hast", "rammstein");
+            //musiquesDance.Add("ich will", "rammstein");
+            //musiquesDance.Add("funkin' for jamaica", "tom browne");
+            //musiquesDance.Add("mambo no. 5", "lou bega");
+            //musiquesDance.Add("stayin' alive", "bee gees");
+            //musiquesDance.Add("magic in the air", "magic system");
+            //musiquesDance.Add("i gotta feeling", "the black eyed peas");
+            //musiquesDance.Add("get lucky", "daft punk");
+            //musiquesDance.Add("safe and sound", "capital cities");
+            return musiquesDance;
         }
     }
 }
