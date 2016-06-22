935b28f974e66675aae607aad9deeeae3823803a
ThibaultCam <cam@intechinfo.fr>
2016-06-22 16:38:19 +0200

    "start Agreggate"

--- Omega/Omega.Model/Agreggation.cs
+++ Omega/Omega.Model/Agreggation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Omega.DataManager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,17 @@ using System.Threading.Tasks;
 
 namespace Omega.Model
 {
-    class Agreggation
+    public class Agreggation
     {
+        Requests cr = new Requests();
+        public List<string> Agreggate(List<string> playlist)
+        {
+            List<string> organisedPlaylist = new List<string>();
+            foreach (string musique in playlist)
+            {
+                CleanTrack analysedSong = cr.GetSongCleanTrack(musique);
+            }
+            return organisedPlaylist;
+        }
     }
 }
