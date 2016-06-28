53b12a56380c28e3ab237007d175c28044f7902b
ThibaultCam <cam@intechinfo.fr>
2016-06-28 14:00:23 +0200

    Fix Agreggate

--- Omega/Omega.Model/Agreggation.cs
+++ Omega/Omega.Model/Agreggation.cs
@@ -1,4 +1,5 @@
-﻿using Omega.DataManager;
+﻿using Newtonsoft.Json.Linq;
+using Omega.DataManager;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,27 +8,34 @@ namespace Omega.Model
     public class Agreggation
     {
         Requests cr = new Requests();
-        public List<string> Agreggate(List<string> playlist)
+        public JArray Agreggate(string playlists)
         {
             List<string> organisedPlaylist = new List<string>();
-            Dictionary<string, double> organised = new Dictionary<string, double>();
-            foreach (string musique in playlist)
+            Dictionary<JToken, double> organised = new Dictionary<JToken, double>();
+            JArray playlistObj = JArray.Parse(playlists);
+            JArray organisedArray = new JArray();
+            string trackIdSource;
+
+            foreach (var playlist in playlistObj)
             {
-                CleanTrack analysedSong = cr.GetSongCleanTrack(musique);
-                if (!organised.ContainsKey(analysedSong.Id))
+                foreach (var track in playlist)
                 {
-                    
-                    organised.Add(analysedSong.Id, double.Parse(analysedSong.Danceability.Replace(".",",")));
+                    trackIdSource = track["RowKey"].ToString().Substring(0, 1) + ":" + track["TrackId"].ToString();
+                    CleanTrack analysedSong = cr.GetSongCleanTrack(trackIdSource);
+                    if (!organised.ContainsKey(track))
+                    {
+                        organised.Add(track, double.Parse(analysedSong.Danceability.Replace(".", ",")));
+                    }
                 }
             }
             var items = from pair in organised
                         orderby pair.Value descending
                         select pair;
-            foreach (KeyValuePair<string,double> track in items)
+            foreach (KeyValuePair<JToken, double> track in items)
             {
-                organisedPlaylist.Add(track.Key);
+                organisedArray.Add(track.Key);
             }
-            return organisedPlaylist;
+            return organisedArray;
         }
     }
 }
